e704bb7da1a01ddb31227baf5231a629b4171402
ngocnhanne <nguyenngocnhan2104@gmail.com>
2023-05-12 01:38:12 +0700

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-app.listen(process.env.PORT, '0.0.0.0');
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
