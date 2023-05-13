416c16b5b589cf3ccba2a887faf1926cfa47e28a
ngocnhanne <nguyenngocnhan2104@gmail.com>
2023-05-13 13:30:30 +0700

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-let server = app.listen(process.env.PORT || 80, function () {
+let server = app.listen(process.env.PORT || 5000, function () {
     logger.api.info(`Server listening on port ${server.address().port}`);
     console.log(`Server listening on port ${server.address().port}`);
   });
